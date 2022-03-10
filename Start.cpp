fa1df4c4f6b1c012148b08eff2bc4924b2304fdd
CaoDuc11 <caoduc2211@gmail.com>
2022-03-10 21:17:54 +0700

    Mở đầu

---
+++ Start.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string d;
+    cout << "What is your name ?";
+    getline(cin , d);
+    cout << "Hello " << d << ". Welcome to my game.";
+}
