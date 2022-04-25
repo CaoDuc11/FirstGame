b530b6783b72f91dd61d68e0dec1136dacf1d079
CaoDuc11 <caoduc2211@gmail.com>
2022-04-25 15:42:09 +0700

    thong so co ban cua game

---
+++ GameBase.h
@@ -0,0 +1,49 @@
+#ifndef GAME_BASE_
+#define GAME_BASE_
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <ctime>
+
+#define SDL_ERROR 1
+#define IMG_ERROR 2
+#define MIX_ERROR 3
+#define TTF_ERROR 4
+
+
+#define MIX_CHANNEL -1
+
+#define TIME_MAX 1000
+#define GROUND 430
+#define MAX_HEIGHT 250
+
+#define BASE_OFFSET_SPEED 0
+
+#define SPEED_INCREASEMENT 2
+#define SCORE_INCREASEMENT 1
+#define TIME_INCREASEMENT 1
+#define FRAME_INCREASEMENT 1
+
+#define GROUND_SPEED 5
+#define ENEMY_SPEED 5
+#define MAX_ENEMY_WIDTH 100
+
+#define IN_AIR_ENEMY 1
+#define ON_GROUND_ENEMY 0
+
+using namespace std;
+const  string WINDOW_TITLE = "FIGHT AND RUN";
+const   int SCREEN_WIDTH = 1280;
+const   int SCREEN_HEIGHT = 720;
+
+void LogError(std::string msg, int error_code = SDL_ERROR);
+
+#endif // !GAME_LIB_
+
--- Start.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    string d;
-    cout << "What is your name ?";
-    getline(cin , d);
-    cout << "Hello " << d << ". Welcome to my game.";
-}
