9eefe3f01e6f5f929e7de888ca35e7b14e7cad8c
CaoDuc11 <caoduc2211@gmail.com>
2022-04-25 17:55:57 +0700

    sua va update cac ham co ban

--- GameBase.h
+++ GameBase.h
@@ -43,7 +43,14 @@ const  string WINDOW_TITLE = "FIGHT AND RUN";
 const   int SCREEN_WIDTH = 1280;
 const   int SCREEN_HEIGHT = 720;
 
-void LogError(std::string msg, int error_code = SDL_ERROR);
+void logSDLError(std::ostream& os, const std::string& msg, bool fatal);
 
-#endif // !GAME_LIB_
+void initSDL(SDL_Window*& window, SDL_Renderer*& renderer);
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+void waitUntilKeyPressed();
+
+SDL_Texture* loadTexture(string path, SDL_Renderer* renderer);
+#endif 
 
